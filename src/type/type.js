7bdf6e18ff82dfbac0e5d7d2ce67f675b593f71b
Stephen Lippa <slippa@Stephens-MacBook-Pro.local>
2020-09-01 15:07:39 -0400

    timer

--- src/type/type.js
+++ src/type/type.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { generate } from "./words/words";
 import { currentTime } from "./time/time";
 import classes from "./type.module.css";
@@ -16,16 +16,30 @@ const Type = () => {
   const [wordCount, setWordCount] = useState(0);
   const [wpm, setWpm] = useState(0);
   const [accuracy, setAccuracy] = useState(0);
-  const [typedChars, setTypedChars] = useState('');
-  TypeHook((key) => {
-    if (!startTime) {
-      setStartTime(currentTime());
-    }
+  const [typedChars, setTypedChars] = useState("");
+  const [counter, setCounter] = React.useState(60);
+  const [completed, setCompleted] = useState(false);
 
+  useEffect(() => {
+    if (startTime) {
+      counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }
+    if (counter === 0) {
+      alert("finito");
+      setCompleted(true);
+    }
+  }, [counter, startTime, completed]);
+
+  TypeHook((key) => {
+    let input = document.getElementById("inputBox");
     let updatedOutgoingChars = outgoingChars;
     let updatedIncomingChars = incomingChars;
 
     if (key === currentChar) {
+      document.getElementById("current").style.backgroundColor = "#09d3ac";
+      if (!startTime) {
+        setStartTime(currentTime());
+      }
       if (leftPadding.length > 0) {
         setLeftPadding(leftPadding.substring(1));
       }
@@ -41,23 +55,28 @@ const Type = () => {
       }
       setIncomingChars(updatedIncomingChars);
 
-      if (incomingChars.charAt(0) === ' ') {
-          setWordCount(wordCount + 1);
-
-          const durationInMinutes = (currentTime() - startTime) / 60000.0;
-
-          setWpm((wordCount+1) / durationInMinutes);
+      if (incomingChars.charAt(0) === " ") {
+        setWordCount(wordCount + 1);
+        const durationInMinutes = (currentTime() - startTime) / 60000.0;
+        setWpm((wordCount + 1) / durationInMinutes);
       }
+
+      if (key === " ") {
+        input.value = "";
+      }
+    }
+
+    if (key !== currentChar) {
+      document.getElementById("current").style.backgroundColor = "red";
     }
 
     const updatedTypedChars = typedChars + key;
     setTypedChars(updatedTypedChars);
-
     setAccuracy(
-        ((updatedOutgoingChars.length * 100) / updatedTypedChars.length).toFixed(
-          2,
-        ),
-      );
+      ((updatedOutgoingChars.length * 100) / updatedTypedChars.length).toFixed(
+        2
+      )
+    );
   });
   return (
     <div className={classes.gameBox}>
@@ -65,11 +84,16 @@ const Type = () => {
         <span className={classes.Out}>
           {(leftPadding + outgoingChars).slice(-20)}
         </span>
-        <span className={classes.Current}>{currentChar}</span>
+        <span id="current" className={classes.Current}>
+          {currentChar}
+        </span>
         <span>{incomingChars.substr(0, 20)}</span>
       </p>
-      <input></input>
-      <h3>WPM : {wpm.toFixed(2)} | ACC: {accuracy}%</h3>
+      <input id="inputBox"></input>
+      <h3>
+        WPM : {wpm.toFixed(2)} | ACC: {accuracy}%
+      </h3>
+      <h4>Countdown: {counter}</h4>
     </div>
   );
 };
