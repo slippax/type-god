3bfcd11e51f6b8fc244390e2aa398927b3bfb744
Stephen Lippa <slippa@Stephens-MacBook-Pro.local>
2020-09-03 19:23:46 -0400

    Updates

---
+++ precache-manifest.2d12bcb48826d7d79d0e5f5e6c7771a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15de7ac680310c8955a0c511b070fe47",
+    "url": "/type-god/index.html"
+  },
+  {
+    "revision": "a28c7df7760b351f8a27",
+    "url": "/type-god/static/css/main.79e0ed7f.chunk.css"
+  },
+  {
+    "revision": "759b17643099f4fbf152",
+    "url": "/type-god/static/js/2.9f8f2bbc.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/type-god/static/js/2.9f8f2bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28c7df7760b351f8a27",
+    "url": "/type-god/static/js/main.f0bfc72d.chunk.js"
+  },
+  {
+    "revision": "cbe68f99f7a8c63ee622",
+    "url": "/type-god/static/js/runtime-main.22e3c501.js"
+  }
+]);
